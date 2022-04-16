2bf142b379e4954c556d63743b1f3383a2d5c728
Iman <i.rezasefat@hamrah-mechanic.com>
2022-04-16 16:21:50 +0430

    init monorepo

---
+++ examples/hm-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Test} from 'hm-core';
+
+function App() {
+  return (
+    <div className="App">
+  <Test/>
+    </div>
+  );
+}
+
+export default App;
