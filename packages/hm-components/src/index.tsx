69b7c2770981365a3dead0bc2dd0367ff62f45dc
satanmourner <s.mahmoudi@hamrah-mechanic.com>
2022-04-20 17:46:47 +0430

    login form created and add fonts and styles

--- packages/hm-components/src/index.tsx
+++ packages/hm-components/src/index.tsx
@@ -1,4 +1,6 @@
 import HelloWorld from './components/HelloWorld';
 import { SimpleButton } from './components/buttons';
+import 'antd/dist/antd.css';
+import './styles/main.scss';
 
 export { HelloWorld, SimpleButton };
--- packages/hm-core/src/auth/LoginForm.tsx
+++ packages/hm-core/src/auth/LoginForm.tsx
@@ -1,11 +1,43 @@
-import { Card, Input } from 'antd';
+import { Card, Form, Input } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { SimpleButton } from 'hm-components';
 
+interface FormData {
+  username: string;
+  password: string;
+}
+
 const LoginForm = () => {
+  const onFinish = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
-    <Card title="ورود به سیستم">
-      <Input />
-      <SimpleButton title="Login" />
+    <Card className="d-flex align-items-center flex-column" title="ورود به سیستم">
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item name="username" rules={[{ required: true, message: 'نام کاربری را لطفا وارد نمایید!' }]}>
+          <Input placeholder="نام کاربری" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'رمز عبور را لطفا وارد نمایید!' }]}
+          className="my-4"
+        >
+          <Input.Password
+            placeholder="رمز عبور"
+            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+          />
+        </Form.Item>
+        <Form.Item>
+          <SimpleButton htmlType="submit" title="ورود" type="primary" block />
+        </Form.Item>
+      </Form>
     </Card>
   );
 };
