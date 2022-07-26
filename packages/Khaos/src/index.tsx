97359a28c2d25bfd5b4d86c23d0d389db2c16439
Iman <i.rezasefat@hamrah-mechanic.com>
2022-07-26 14:23:10 +0430

    filters and pagination added to context

--- packages/Khaos/src/index.tsx
+++ packages/Khaos/src/index.tsx
@@ -1,6 +1,7 @@
 import { SimpleButton, TabsSelect } from 'hm-components';
-import { Khaos, Resource, request, LoginForm, authenticate } from 'hm-core';
+import { Khaos, Resource, request, LoginForm, authenticate, Filters } from 'hm-core';
 import 'hm-components/dist/index.css';
 
 //EXPORTS
 export { SimpleButton, TabsSelect, Khaos, Resource, request, LoginForm, authenticate };
+export type { Filters };
--- packages/hm-core/src/index.tsx
+++ packages/hm-core/src/index.tsx
@@ -3,8 +3,9 @@ import { request } from './api/requestHandler';
 import Khaos from './_app';
 import { Resource, ResorceContext } from './core';
 import 'antd/dist/antd.min.css';
+import { Filters } from './types';
 
 //EXPORTS
 export { authenticate } from './auth/authUtilities';
-export * from './types';
+export type { Filters };
 export { AuthProvider, AuthContext, LoginForm, request, Khaos, Resource, ResorceContext };
